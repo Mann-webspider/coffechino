f66a9304cf6dc10cf1cb977468bb35f733e4b67c
manndalsania <61105721+Mann-webspider@users.noreply.github.com>
2022-09-21 16:15:27 +0530

    main

---
+++ script.js
@@ -0,0 +1,10 @@
+var twitter = document.getElementById("twi");
+var instagram = document.getElementById("ins");
+twitter.addEventListener("click", red);
+instagram.addEventListener("click", blue);
+function red() {
+  window.location.href = "https://twitter.com/MannDalsaniya07";
+}
+function blue() {
+  window.location.href = "https://instagram.com/ig_unconscious_boy";
+}
